b0a983eba118ea18f02652c233dab92b8722f4ac
nick_ablan <nicholasablan@gmail.com>
2022-04-04 21:46:22 -0400

    updated changes

--- db/index.js
+++ db/index.js
@@ -322,6 +322,21 @@ async function getAllTags() {
 }
 
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
 module.exports = {  
   client,
   createUser,
@@ -336,5 +351,6 @@ module.exports = {
   createTags,
   getAllTags,
   createPostTag,
-  addTagsToPost
+  addTagsToPost,
+  getUserByUsername
 }
